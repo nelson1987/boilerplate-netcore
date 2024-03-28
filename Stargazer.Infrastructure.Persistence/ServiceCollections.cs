613af000241806aab019f67ff2122b027d6793e6
Nelson Da Silva Neto <nelson.neto@stone.com.br>
2024-03-28 15:04:54 -0300

    Include Persistence

---
+++ Stargazer.Infrastructure.Persistence/ServiceCollections.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Stargazer.Domain.Entities;
+
+namespace Stargazer.Infrastructure.Persistence;
+public static class ServiceCollections
+{
+    public static void AddPersistence(this IServiceCollection services/*, IConfiguration configuration*/)
+    {
+        services.AddTransient(typeof(IGenericRepositoryAsync<>), typeof(GenericRepositoryAsync<>));
+        services.AddTransient<IProductRepositoryAsync, ProductRepositoryAsync>();
+    }
+
+    public interface IGenericRepositoryAsync<T> where T : class
+    {
+        Task<T> SelectAsync();
+    }
+
+    public class GenericRepositoryAsync<T> : IGenericRepositoryAsync<T> where T : class
+    {
+        public Task<T> SelectAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IProductRepositoryAsync
+    {
+        Task<bool> IsWarming();
+
+    }
+
+    public class ProductRepositoryAsync : GenericRepositoryAsync<WeatherForecast>, IProductRepositoryAsync
+    {
+        public Task<bool> IsWarming()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Stargazer.WebApi/Program.cs
+++ Stargazer.WebApi/Program.cs
@@ -1,9 +1,10 @@
+using Stargazer.Infrastructure.Persistence;
 using Stargazer.WebApi.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddPersistence(builder.Configuration);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
